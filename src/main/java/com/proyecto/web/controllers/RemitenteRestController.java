3d0dced290336a689c470ffb89f14cd4e635a04d
nelrsf <nel.rsf@gmail.com>
2020-10-09 16:46:01 -0500

    terminado restcontroller remitente

--- src/main/java/com/proyecto/web/controllers/RemitenteRestController.java
+++ src/main/java/com/proyecto/web/controllers/RemitenteRestController.java
@@ -3,10 +3,16 @@ package com.proyecto.web.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.proyecto.web.models.entity.Remitente;
@@ -29,4 +35,33 @@ public class RemitenteRestController {
 	public Remitente show(@PathVariable Long id) {
 		return this.remitenteService.findById(id);
 	}
+	
+	@PostMapping("/remitente")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Remitente create(@RequestBody Remitente remitente) {
+		this.remitenteService.save(remitente);
+		return remitente;
+	}
+	
+	
+	@PutMapping("/remitente/{id}")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Remitente update(@RequestBody Remitente remitente, @PathVariable long id) {
+		Remitente actualRemitente = this.remitenteService.findById(id);
+		actualRemitente.setNombre(remitente.getNombre());
+		actualRemitente.setApellido(remitente.getApellido());
+		actualRemitente.setDireccion(remitente.getDireccion());
+		actualRemitente.setTelefono(remitente.getTelefono());
+		this.remitenteService.save(actualRemitente);
+		return actualRemitente;
+	}
+	
+	@DeleteMapping("/remitente/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void delete(@PathVariable long id) {
+		Remitente actualRemitente = this.remitenteService.findById(id);
+		this.remitenteService.delete(actualRemitente);
+	}
+	
+	
 }
