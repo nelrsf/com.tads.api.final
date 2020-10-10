86637a55ce97023a29c65ab8f69aa5bbe9f50863
nelrsf <nel.rsf@gmail.com>
2020-10-10 10:53:11 -0500

    fix2 RemitenteRestController

--- src/main/java/com/proyecto/web/controllers/RemitenteRestController.java
+++ src/main/java/com/proyecto/web/controllers/RemitenteRestController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.proyecto.web.models.entity.Remitente;
 import com.proyecto.web.models.services.IRemitenteService;
 
-@CrossOrigin(origins= {"*"})
+@CrossOrigin(origins= {"https://frontend-final-tads.web.app"})
 @RestController
 @RequestMapping("/api")
 public class RemitenteRestController {
